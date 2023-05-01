2da359c4b604c74d349d1ea592bdc3e3faea5c12
mohammadreza kari <mohammadreza.k@780.ir>
2023-05-02 01:05:33 +0330

    remove redundant code

--- pkg/postgresql/postgres.go
+++ pkg/postgresql/postgres.go
@@ -13,8 +13,8 @@ var (
 
 func Get(cnf *config.EnvConfig) (err error, sDB *gorm.DB) {
 	if db == nil {
-		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
-			cnf.Database.Host,
+		dsn := fmt.Sprintf("host=postgres user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+			//cnf.Database.Host,
 			cnf.Database.User,
 			cnf.Database.Password,
 			cnf.Database.DBName,
